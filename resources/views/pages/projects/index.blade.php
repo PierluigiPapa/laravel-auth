21418d83a8a13ae7efde5d3ef50a6294f6ad94ce
PierluigiPapa <pierluigipapa9@gmail.com>
2024-03-25 17:36:29 +0100

    feat: print table on index.blade.php

--- resources/views/pages/projects/index.blade.php
+++ resources/views/pages/projects/index.blade.php
@@ -6,4 +6,27 @@
     <h1 class="text-center py-3">PierFolio</h1>
 </main>
 
+<table class="table">
+    <thead>
+      <tr>
+        <th scope="col">Id</th>
+        <th scope="col">Titolo</th>
+        <th scope="col">Descrizione</th>
+        <th scope="col">Cover</th>
+        <th scope="col">Slug</th>
+      </tr>
+    </thead>
+    <tbody>
+        @foreach ($project as  $item)
+        <tr>
+            <td>{{$item->id}}</td>
+            <td>{{$item->title}}</td>
+            <td>{{$item->content}}</td>
+            <td>{{$item->cover}}</td>
+            <td>{{$item->slug}}</td>
+        </tr>
+        @endforeach
+    </tbody>
+  </table>
+
 @endsection
