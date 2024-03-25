fabd371ae2f71b6c8859cceeae5b9e148853739b
PierluigiPapa <pierluigipapa9@gmail.com>
2024-03-25 17:49:25 +0100

    fix: add button delete and edit

--- resources/views/pages/projects/index.blade.php
+++ resources/views/pages/projects/index.blade.php
@@ -4,29 +4,39 @@
 
 <main class="container">
     <h1 class="text-center py-3">PierFolio</h1>
-</main>
 
-<table class="table">
-    <thead>
-      <tr>
-        <th scope="col">Id</th>
-        <th scope="col">Titolo</th>
-        <th scope="col">Descrizione</th>
-        <th scope="col">Cover</th>
-        <th scope="col">Slug</th>
-      </tr>
-    </thead>
-    <tbody>
-        @foreach ($project as  $item)
-        <tr>
-            <td>{{$item->id}}</td>
-            <td>{{$item->title}}</td>
-            <td>{{$item->content}}</td>
-            <td>{{$item->cover}}</td>
-            <td>{{$item->slug}}</td>
-        </tr>
-        @endforeach
-    </tbody>
-  </table>
+    <a class="btn btn-primary mb-3" href="{{ route('dashboard.projects.create') }}">Crea</a>
+
+    <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">Id</th>
+            <th scope="col">Titolo</th>
+            <th scope="col">Descrizione</th>
+            <th scope="col">Cover</th>
+            <th scope="col">Slug</th>
+          </tr>
+        </thead>
+        <tbody>
+            @foreach ($project as $item)
+            <tr>
+                <td>{{ $item->id }}</td>
+                <td>{{ $item->title }}</td>
+                <td>{{ $item->content }}</td>
+                <td>{{ $item->cover }}</td>
+                <td>{{ $item->slug }}</td>
+                <td>
+                    <a class="btn btn-primary" href="{{ route('dashboard.projects.edit', $item->id) }}">Modifica</a>
+                    <form method="POST" action="{{ route('dashboard.projects.destroy', $item->id) }}" style="display: inline-block;">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="btn btn-danger">Delete</button>
+                    </form>
+                </td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+</main>
 
 @endsection
